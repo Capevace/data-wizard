e1eaa21ee114726c966717157c58feeb5c379ce1
Lukas Mateffy <github@mateffy.me>
2025-03-13 19:12:27 +0100

    fix: some settings update for docker to work

--- config/async.php
+++ config/async.php
@@ -3,4 +3,9 @@
 return [
     // Set the async timeout to something large as LLM calls can take a long time.
     'timeout' => 600,
+
+    /*
+     * Default output length of async processes.
+     */
+    'defaultOutputLength' => 1024 * 100,
 ];
