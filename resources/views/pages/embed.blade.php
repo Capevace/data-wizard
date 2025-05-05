d20090f43231ea1556bc40bd71ebc63a2ac6d523
Lukas Mateffy <github@mateffy.me>
2025-05-05 21:33:59 +0200

    chore: asset build

--- resources/views/pages/embed.blade.php
+++ resources/views/pages/embed.blade.php
@@ -1,10 +1,14 @@
+@props([
+    'extractorId'
+])
+
 <x-layouts.app>
     <script
-        src="//data-wizard.test/vendor/wire-elements/wire-extender.js"
+        src="{{ asset('vendor/wire-elements/wire-extender.js') }}"
         data-livewire-asset-uri="{{ asset('vendor/livewire/livewire.min.js') }}"
     ></script>
     <div>
-        <livewire data-component="embedded-extractor" data-params='@json(['extractorId' => '0e6961fb-3064-450d-be86-1a5c5e68d0cc'])'>
+        <livewire data-component="embedded-extractor" data-params='@json(['extractorId' => $extractorId])'>
     </livewire>
     </div>
 </x-layouts.app>
